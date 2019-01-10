2bc13cde4550ec40ec54b3b72a67f5311897d7b4
Nianhong Jiao <1327362838@qq.com>
2019-01-10 16:10:02 +0800

    Update fitPose2d.py

--- fitPose2d.py
+++ fitPose2d.py
@@ -20,6 +20,22 @@ parser = argparse.ArgumentParser()
 parser.add_argument('idx', type=int)
 args = parser.parse_args()
 
+
+# Mapping from LSP joints to SMPL joints.
+# 0 Right ankle  8
+# 1 Right knee   5
+# 2 Right hip    2
+# 3 Left hip     1
+# 4 Left knee    4
+# 5 Left ankle   7
+# 6 Right wrist  21
+# 7 Right elbow  19
+# 8 Right shoulder 17
+# 9 Left shoulder  16
+# 10 Left elbow    18
+# 11 Left wrist    20
+# 12 Neck           -
+# 13 Head top       added
 def fit(img, j2ds, prior_path, model_dir, gen='n', camProject=None):
 	smpl_joints_ids = [8, 5, 2, 1, 4, 7, 21, 19, 17, 16, 18, 20, 24]
 	torso_ids = [2, 3, 8, 9]
